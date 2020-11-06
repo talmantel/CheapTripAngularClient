f4e0a3359e2f42d0bd9b796c837f29bda28a9e90
Olga Zaleskovskaya <oz.flexo@gmail.com>
2020-11-06 08:17:20 +0200

    with build version

--- cheapTrip/src/environments/environment.ts
+++ cheapTrip/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  urlAppachi: 'http://52.14.161.122:8080/',
-  urlTomCat:  'http://3.23.159.104:3333/'
+  urlAppachi: 'http://3.18.167.68:8080/',
+  urlTomCat:  'http://3.23.159.104:8080/'
 
 };
 
