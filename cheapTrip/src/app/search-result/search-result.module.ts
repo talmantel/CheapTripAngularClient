bf6867bcaa3a819d721dfdb5c2e6f4858c598a0f
Mehmet <mmtkalayci@gmail.com>
2022-01-23 20:57:03 +0300

    search-result-component removed. it was containing the google form

--- cheapTrip/src/app/search-result/search-result.module.ts
+++ cheapTrip/src/app/search-result/search-result.module.ts
@@ -4,17 +4,10 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { SearchResultComponent } from './search-result.component';
 import { PathDetailsComponent } from './path-details/path-details.component';
-import { PathMapComponent } from './path-map/path-map.component';
-import { CapitalFirstLetterPipe } from './path-details/capitalFirstLetter.pipe';
 import { SearchResultRoutingModule } from './search-result-routing.module';
-import { LogoComponent } from '../logo/logo.component';
 
 @NgModule({
-  declarations: [
-    SearchResultComponent,
-    PathDetailsComponent,
-    CapitalFirstLetterPipe,
-    PathMapComponent],
+  declarations: [SearchResultComponent, PathDetailsComponent],
   imports: [CommonModule, MaterialModule, SearchResultRoutingModule],
 })
 export class SearchResultModule {}
