508503f163c629bcdf6df9e41a687437d748be01
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2023-03-22 11:45:06 +0200

    Update header.component.ts

--- cheapTrip/src/app/header/header/header.component.ts
+++ cheapTrip/src/app/header/header/header.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import * as TripDirectionActions from '../../trip-direction/store/trip-direction.actions';
-import { HttpClient } from '@angular/common/http';
 
 import * as fromApp from '../../store/app.reducer';
 import { Modes } from 'src/app/trip-direction/trip-direction.model';
@@ -18,24 +17,10 @@ export class HeaderComponent implements OnInit {
   isLoading: boolean;
   subscription: Subscription;
 
-  constructor(
-    private route: Router,
-    private http: HttpClient,
-    private store: Store<fromApp.AppState>
-  ) {}
+  constructor(private route: Router, private store: Store<fromApp.AppState>) {}
   isDarkTheme = false;
 
   ngOnInit(): void {}
-
-  ngAfterViewInit() {
-    console.log('HEADER PAGE');
-    this.http
-      .get('assets/new_json/transportation_types.json')
-      .subscribe(data => {
-        console.log('TRANSPORT', data);
-        sessionStorage.setItem('myData', JSON.stringify(data));
-      });
-  }
 
   toHomePage(): void {
     this.store.dispatch(new TripDirectionActions.GoHome());
