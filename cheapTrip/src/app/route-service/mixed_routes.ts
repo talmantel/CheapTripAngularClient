86363271f7f9b2cb9e52859bec50b7c281255308
ltlaitoff <ltlaitoff@gmail.com>
2023-05-14 17:47:32 +0300

    feat(route-service/mixed_routes): Rewrited mixed_routes to new json standarts

--- cheapTrip/src/app/route-service/mixed_routes.ts
+++ cheapTrip/src/app/route-service/mixed_routes.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {
   IJsonTravelData,
-  IJsonRoutData,
+  IJsonPartlyRoute,
+  IJsonPartlyRouteItem,
 } from '../trip-direction/trip-direction.model';
 import { HttpClient } from '@angular/common/http';
 
@@ -11,75 +12,48 @@ import { HttpClient } from '@angular/common/http';
 export class MixedRoutes {
   constructor(private http: HttpClient) {}
 
-  // getFilterJson({
-  //   startPoint,
-  //   endPoint,
-  // }: {
-  //   startPoint: string;
-  //   endPoint: string;
-  // }): Promise<any> {
-  //   const pathData: IJsonTravelData[] = [];
-
-  //   return caches
-  //     .match(new Request('assets/new_json/routes.json'))
-  //     .then(response => {
-  //       if (response) {
-  //         return response.json();
-  //       }
-  //     })
-  //     .then(mixedData => {
-  //       const filterData = mixedData[`${startPoint}0${endPoint}`];
-  //       if (!filterData) return [];
-  //       const paths = filterData.direct_routes.split(',');
-  //       return caches
-  //         .match(new Request('assets/new_json/direct_routes.json'))
-  //         .then(response => {
-  //           if (response) {
-  //             return response.json();
-  //           }
-  //         })
-  //         .then(data => {
-  //           paths.forEach((id: string): void => {
-  //             pathData.push(data[id]);
-  //           });
-  //           filterData.travel_data = pathData;
-  //           return filterData;
-  //         });
-  //     });
-  // }
-
   getFilterJson({
     startPoint,
     endPoint,
   }: {
     startPoint: string;
     endPoint: string;
-  }): Promise<any> {
+  }): Promise<IJsonPartlyRouteItem | null> {
     const pathData: IJsonTravelData[] = [];
+    const JSON_FOLDER_NAME = 'json';
     console.time('GetFilterJson Mixed_Data');
 
     return this.http
-      .get<any>(`assets/new_json/partly/routes/${startPoint}.json`)
-      .toPromise()
-      .then(flyingData => {
+      .get<IJsonPartlyRoute>(
+        `assets/${JSON_FOLDER_NAME}/partly/routes/${startPoint}.json`
+      )
+      .toPromise<IJsonPartlyRoute>()
+      .then((flyingData): Promise<IJsonPartlyRouteItem | null> => {
         const filterData = flyingData[`${endPoint}`];
-        if (!filterData) return [];
-        const path: [] = filterData.direct_routes.split(',');
+        if (!filterData) return null;
+
+        const path: string[] = filterData.direct_routes;
+
         return caches.match('direct_routes').then(response => {
           if (response) {
             return response.json().then(data => {
               path.forEach((id: string): void => {
                 pathData.push(data[id]);
               });
+
               filterData.travel_data = pathData;
               console.timeEnd('GetFilterJson Mixed_Data');
+
               return filterData;
             });
           }
         });
+
+        return null;
       })
       .catch(error => {
         console.error('Error:', error);
+        return null;
       });
   }
 
@@ -91,7 +65,7 @@ export class MixedRoutes {
     console.time('Get_Mixed_Routes');
 
     return this.getFilterJson({ startPoint, endPoint }).then(data => {
-      if (data.length !== 0) {
+      if (data !== null) {
         const result = [];
 
         const directPaths = data.travel_data.map(el => ({
