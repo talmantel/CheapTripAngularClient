3779e8044f35d8eaa0ce4951f550791d91a39486
ltlaitoff <ltlaitoff@gmail.com>
2023-05-14 17:31:00 +0300

    feat(route-service/flying_routes): Rewrited to new json standart

--- cheapTrip/src/app/route-service/flying_routes.ts
+++ cheapTrip/src/app/route-service/flying_routes.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {
   IJsonTravelData,
-  IJsonRoutData,
+  IJsonPartlyRoute,
+  IJsonPartlyRouteItem,
 } from '../trip-direction/trip-direction.model';
 import { HttpClient } from '@angular/common/http';
 
@@ -17,48 +18,65 @@ export class FlyingRoutes {
   }: {
     startPoint: string;
     endPoint: string;
-  }): Promise<any> {
+  }): Promise<IJsonPartlyRouteItem | null> {
     const pathData: IJsonTravelData[] = [];
+    const JSON_FOLDER_NAME = 'json';
+
     console.time('GetFilterJson Flying_Data');
 
     return this.http
-      .get<any>(`assets/new_json/partly/flying_routes/${startPoint}.json`)
-      .toPromise()
-      .then(flyingData => {
+      .get<IJsonPartlyRoute>(
+        `assets/${JSON_FOLDER_NAME}/partly/flying_routes/${startPoint}.json`
+      )
+      .toPromise<IJsonPartlyRoute>()
+      .then((flyingData): Promise<IJsonPartlyRouteItem | null> => {
         const filterData = flyingData[`${endPoint}`];
 
-        if (!filterData) return [];
-        const path: [] = filterData.direct_routes.split(',');
+        if (!filterData) return Promise.resolve(null);
+
+        const path: string[] = filterData.direct_routes;
+
+        console.log('%c⧭', 'color: #f1f1f6', path);
+
         return caches.match('direct_routes').then(response => {
           if (response) {
             return response.json().then(data => {
               path.forEach((id: string): void => {
                 pathData.push(data[id]);
               });
+
               filterData.travel_data = pathData;
+
               console.timeEnd('GetFilterJson Flying_Data');
               console.log('filterData', filterData);
+
               return filterData;
             });
           }
+
+          return null;
         });
       })
       .catch(error => {
         console.error('Error Fly:', error);
+
+        return null;
       });
   }
 
-  async getTravelData(startPoint: string, endPoint: string): Promise<any> {
+  async getTravelData(startPoint: string, endPoint: string) {
     console.time('GetTransportAndLocationFlying');
     const transportType: {} = JSON.parse(
       sessionStorage.getItem('transportationTypes')
     );
+
     const locations: {} = JSON.parse(sessionStorage.getItem('locations'));
     console.timeEnd('GetTransportAndLocationFlying');
 
     return this.getFilterJson({ startPoint, endPoint }).then(data => {
       console.log('data', data);
-      if (data && data.length !== 0) {
+
+      if (data && data !== null) {
         const result = [];
 
         const directPaths = data.travel_data.map(el => ({
@@ -68,6 +86,7 @@ export class FlyingRoutes {
           to: locations[el.to].name,
           transportation_type: transportType[el.transport].name,
         }));
+
         result.push({
           duration_minutes: data.duration,
           euro_price: data.price,
@@ -76,9 +95,9 @@ export class FlyingRoutes {
         });
 
         return result;
-      } else {
-        return [];
       }
+
+      return [];
     });
   }
 }
