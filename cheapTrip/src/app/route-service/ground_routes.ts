78b1de8cac5f608f7abf8b8551334f5096ab0f04
ltlaitoff <ltlaitoff@gmail.com>
2023-05-14 17:40:55 +0300

    feat(route-service/ground_routes): Migrated ground_routes to new json standart

--- cheapTrip/src/app/route-service/ground_routes.ts
+++ cheapTrip/src/app/route-service/ground_routes.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {
   IJsonTravelData,
-  IJsonRoutData,
+  IJsonPartlyRoute,
+  IJsonPartlyRouteItem,
 } from '../trip-direction/trip-direction.model';
 import { HttpClient } from '@angular/common/http';
 
@@ -11,78 +12,51 @@ import { HttpClient } from '@angular/common/http';
 export class GroundRoutes {
   constructor(private http: HttpClient) {}
 
-  // getFilterJson({
-  //   startPoint,
-  //   endPoint,
-  // }: {
-  //   startPoint: string;
-  //   endPoint: string;
-  // }): Promise<any> {
-  //   const pathData: IJsonTravelData[] = [];
-  //   console.time('Get_Ground_Routes');
-  //   return caches
-  //     .match(new Request('assets/new_json/fixed_routes.json'))
-  //     .then(response => {
-  //       if (response) {
-  //         return response.json();
-  //       }
-  //     })
-  //     .then(fixedData => {
-  //       console.timeEnd('Get_Ground_Routes');
-
-  //       const filterData = fixedData[`${startPoint}0${endPoint}`];
-  //       if (!filterData) return [];
-  //       const path = filterData.direct_routes.split(',');
-  //       return caches
-  //         .match(new Request('assets/new_json/direct_routes.json'))
-  //         .then(response => {
-  //           if (response) {
-  //             return response.json();
-  //           }
-  //         })
-  //         .then(data => {
-  //           path.forEach((id: string): void => {
-  //             pathData.push(data[id]);
-  //           });
-  //           filterData.travel_data = pathData;
-
-  //           return filterData;
-  //         });
-  //     });
-  // }
-
   getFilterJson({
     startPoint,
     endPoint,
   }: {
     startPoint: string;
     endPoint: string;
-  }): Promise<any> {
+  }): Promise<IJsonPartlyRouteItem | null> {
     const pathData: IJsonTravelData[] = [];
+    const JSON_FOLDER_NAME = 'json';
+
     console.time('GetFilterJson Ground_Data');
 
     return this.http
-      .get<any>(`assets/new_json/partly/fixed_routes/${startPoint}.json`)
-      .toPromise()
-      .then(flyingData => {
+      .get<IJsonPartlyRoute>(
+        `assets/${JSON_FOLDER_NAME}/partly/fixed_routes/${startPoint}.json`
+      )
+      .toPromise<IJsonPartlyRoute>()
+      .then((flyingData): Promise<IJsonPartlyRouteItem | null> => {
         const filterData = flyingData[`${endPoint}`];
-        if (!filterData) return [];
-        const path: [] = filterData.direct_routes.split(',');
+
+        if (!filterData) return null;
+
+        const path: string[] = filterData.direct_routes;
+
         return caches.match('direct_routes').then(response => {
           if (response) {
             return response.json().then(data => {
               path.forEach((id: string): void => {
                 pathData.push(data[id]);
               });
+
               filterData.travel_data = pathData;
               console.timeEnd('GetFilterJson Ground_Data');
+
               return filterData;
             });
           }
+
+          return null;
         });
       })
       .catch(error => {
         console.error('Error:', error);
+
+        return null;
       });
   }
 
@@ -96,7 +70,7 @@ export class GroundRoutes {
 
     console.time('Ground-Routes');
     return this.getFilterJson({ startPoint, endPoint }).then(data => {
-      if (data.length !== 0) {
+      if (data !== null) {
         const result = [];
         const directPaths = data.travel_data.map(el => ({
           duration_minutes: el.duration,
@@ -105,18 +79,20 @@ export class GroundRoutes {
           to: locations[el.to].name,
           transportation_type: transportType[el.transport].name,
         }));
+
         result.push({
           duration_minutes: data.duration,
           euro_price: data.price,
           route_type: 'ground_routes',
           direct_paths: directPaths,
         });
+
         console.timeEnd('Ground-Routes');
 
         return result;
-      } else {
-        return [];
       }
+
+      return [];
     });
   }
 }
